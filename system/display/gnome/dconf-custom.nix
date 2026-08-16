07b7bb63cbf19f19d00ca1d45e62ced95eb7d30b
huon <huonswales@gmail.com>
2026-08-16 17:47:58 +1000

    Flake update

--- system/display/gnome/dconf-custom.nix
+++ system/display/gnome/dconf-custom.nix
@@ -4,30 +4,69 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    # force minimise and maximise buttons
+    "org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
+    # fix bugs in dconf output parsing
     "org/gnome/shell/extensions/rounded-window-corners-reborn" = lib.mkForce {
       blacklist = [ "ulauncher" ];
       border-width = 0;
 
       global-rounded-corner-settings = [
-        (mkDictionaryEntry [ "padding" (mkVariant [
-          (mkDictionaryEntry [ "left"   (mkUint32 0) ])
-          (mkDictionaryEntry [ "right"  (mkUint32 0) ])
-          (mkDictionaryEntry [ "top"    (mkUint32 0) ])
-          (mkDictionaryEntry [ "bottom" (mkUint32 0) ])
-        ]) ])
-        (mkDictionaryEntry [ "keepRoundedCorners" (mkVariant [
-          (mkDictionaryEntry [ "maximized"  false ])
-          (mkDictionaryEntry [ "fullscreen" false ])
-        ]) ])
-        (mkDictionaryEntry [ "borderRadius" (mkVariant (mkUint32 15)) ])
-        (mkDictionaryEntry [ "smoothing"    (mkVariant 0.3) ])
-        (mkDictionaryEntry [ "borderColor"  (mkVariant (mkTuple [
-          0.48627451062202454
-          0.9921568632125854
-          0.9960784316062927
-          1.0
-        ])) ])
-        (mkDictionaryEntry [ "enabled" (mkVariant true) ])
+        (mkDictionaryEntry [
+          "padding"
+          (mkVariant [
+            (mkDictionaryEntry [
+              "left"
+              (mkUint32 0)
+            ])
+            (mkDictionaryEntry [
+              "right"
+              (mkUint32 0)
+            ])
+            (mkDictionaryEntry [
+              "top"
+              (mkUint32 0)
+            ])
+            (mkDictionaryEntry [
+              "bottom"
+              (mkUint32 0)
+            ])
+          ])
+        ])
+        (mkDictionaryEntry [
+          "keepRoundedCorners"
+          (mkVariant [
+            (mkDictionaryEntry [
+              "maximized"
+              false
+            ])
+            (mkDictionaryEntry [
+              "fullscreen"
+              false
+            ])
+          ])
+        ])
+        (mkDictionaryEntry [
+          "borderRadius"
+          (mkVariant (mkUint32 15))
+        ])
+        (mkDictionaryEntry [
+          "smoothing"
+          (mkVariant 0.3)
+        ])
+        (mkDictionaryEntry [
+          "borderColor"
+          (mkVariant (mkTuple [
+            0.48627451062202454
+            0.9921568632125854
+            0.9960784316062927
+            1.0
+          ]))
+        ])
+        (mkDictionaryEntry [
+          "enabled"
+          (mkVariant true)
+        ])
       ];
 
       settings-version = mkUint32 9;
--- system/specialisations.nix
+++ system/specialisations.nix
@@ -7,79 +7,79 @@
   # default specialisations contains gnome and gdm
   imports = [
     ./display/gnome/gdm.nix
-    # ./display/hyprland
+    ./display/hyprland
   ];
 
   specialisation = {
-    # tron.configuration = {
-    #   # disable gdm and gnome desktop manager
-    #   services.displayManager.gdm.enable = lib.mkForce false;
-    #   services.desktopManager.gnome.enable = lib.mkForce false;
+    tron.configuration = {
+      # disable gdm and gnome desktop manager
+      services.displayManager.gdm.enable = lib.mkForce false;
+      services.desktopManager.gnome.enable = lib.mkForce false;
 
-    #   # Fix error:
-    #   # since you installed Home Manager via its NixOS module and
-    #   #  'home-manager.useUserPackages' is enabled, you need to add
+      # Fix error:
+      # since you installed Home Manager via its NixOS module and
+      #  'home-manager.useUserPackages' is enabled, you need to add
 
-    #   environment.pathsToLink = [
-    #     "/share/applications"
-    #     "/share/xdg-desktop-portal"
-    #   ];
+      environment.pathsToLink = [
+        "/share/applications"
+        "/share/xdg-desktop-portal"
+      ];
 
-    #   #  to your NixOS configuration so that the portal definitions and DE
-    #   #  provided configurations get linked.
+      #  to your NixOS configuration so that the portal definitions and DE
+      #  provided configurations get linked.
 
-    #   # setup hyprland with home-manager
-    #   home-manager.users.huon = {
-    #     home = {
-    #       sessionVariables.NIXOS_OZONE_WL = "1";
-    #       packages = with pkgs; [
-    #         dconf
-    #         udis86
-    #       ];
-    #     };
-    #     programs.kitty.enable = true;
-    #     wayland.windowManager.hyprland = {
-    #       enable = true;
-    #       configType = "lua";
-    #       settings = {
-    #         config = {
-    #           general = {
-    #             gaps_in = 5;
-    #             gaps_out = 20;
-    #             border_size = 2;
-    #           };
+      # setup hyprland with home-manager
+      home-manager.users.huon = {
+        home = {
+          sessionVariables.NIXOS_OZONE_WL = "1";
+          packages = with pkgs; [
+            dconf
+            udis86
+          ];
+        };
+        programs.kitty.enable = true;
+        wayland.windowManager.hyprland = {
+          enable = true;
+          configType = "lua";
+          settings = {
+            config = {
+              general = {
+                gaps_in = 5;
+                gaps_out = 20;
+                border_size = 2;
+              };
 
-    #           decoration = {
-    #             rounding = 10;
-    #           };
-    #         };
+              decoration = {
+                rounding = 10;
+              };
+            };
 
-    #         monitor = [
-    #           {
-    #             output = "HDMI-A-1";
-    #             mode = "1920x1080@60";
-    #             position = "0x0";
-    #             scale = 1;
-    #           }
-    #           {
-    #             output = "eDP-1";
-    #             mode = "1920x1080@144.00301";
-    #             position = "1920x0";
-    #             scale = 1;
-    #           }
-    #         ];
+            monitor = [
+              {
+                output = "HDMI-A-1";
+                mode = "1920x1080@60";
+                position = "0x0";
+                scale = 1;
+              }
+              {
+                output = "eDP-1";
+                mode = "1920x1080@144.00301";
+                position = "1920x0";
+                scale = 1;
+              }
+            ];
 
-    #         bind = [
-    #           {
-    #             _args = [
-    #               "SUPER + RETURN"
-    #               (lib.generators.mkLuaInline "lh.dsp.exec_cmd(\"kitty\")")
-    #             ];
-    #           }
-    #         ];
-    #       };
-    #     };
-    #   };
-    # };
+            bind = [
+              {
+                _args = [
+                  "SUPER + RETURN"
+                  (lib.generators.mkLuaInline "lh.dsp.exec_cmd(\"kitty\")")
+                ];
+              }
+            ];
+          };
+        };
+      };
+    };
   };
 }
